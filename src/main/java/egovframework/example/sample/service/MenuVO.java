a95c2ed392b7e7e0af2e431dcaed41a67ee4e5d8
PC <PC@DESKTOP-4T4RCSB>
2023-06-27 17:38:09 +0900

    menu vo add

---
+++ src/main/java/egovframework/example/sample/service/MenuVO.java
@@ -0,0 +1,34 @@
+package egovframework.example.sample.service;
+
+public class MenuVO extends SampleDefaultVO {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7937724087135444948L;
+	
+	/** 메뉴 id */
+	private String menuId;
+
+	/** 메뉴명 */
+	private String menuNm;
+	
+	public String getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getMenuNm() {
+		return menuNm;
+	}
+
+	public void setMenuNm(String menuNm) {
+		this.menuNm = menuNm;
+	}
+	
+	
+	
+}
