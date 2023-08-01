94bb7a0659ee96478340ca751a3510e04eab7122
PC <PC@DESKTOP-4T4RCSB>
2023-08-01 17:27:57 +0900

    222

---
+++ src/main/java/egovframework/program/reserve/service/GolfSettingVO.java
@@ -0,0 +1,127 @@
+package egovframework.program.reserve.service;
+
+public class GolfSettingVO {
+
+	/** 홀갯수 */
+	private String holeNum;
+	
+	/** A코스명 */
+	private String aCourseNm;
+	
+	/** B코스명 */
+	private String bCourseNm;
+	
+	/** C코스명 */
+	private String cCourseNm;
+	
+	/** D코스명 */
+	private String dCourseNm;
+	
+	/** 1부시작시간 */
+	private String part1SrtTime;
+	
+	/** 1부종료시간 */
+	private String part1EndTime;
+	
+	/** 2부시작시간 */
+	private String part2SrtTime;
+	
+	/** 2부종료시간 */
+	private String part2EndTime;
+	
+	/** 3부시작시간 */
+	private String part3SrtTime;
+	
+	/** 3부종료시간 */
+	private String part3EndTime;
+
+	public String getHoleNum() {
+		return holeNum;
+	}
+
+	public void setHoleNum(String holeNum) {
+		this.holeNum = holeNum;
+	}
+
+	public String getaCourseNm() {
+		return aCourseNm;
+	}
+
+	public void setaCourseNm(String aCourseNm) {
+		this.aCourseNm = aCourseNm;
+	}
+
+	public String getbCourseNm() {
+		return bCourseNm;
+	}
+
+	public void setbCourseNm(String bCourseNm) {
+		this.bCourseNm = bCourseNm;
+	}
+
+	public String getcCourseNm() {
+		return cCourseNm;
+	}
+
+	public void setcCourseNm(String cCourseNm) {
+		this.cCourseNm = cCourseNm;
+	}
+
+	public String getdCourseNm() {
+		return dCourseNm;
+	}
+
+	public void setdCourseNm(String dCourseNm) {
+		this.dCourseNm = dCourseNm;
+	}
+
+	public String getPart1SrtTime() {
+		return part1SrtTime;
+	}
+
+	public void setPart1SrtTime(String part1SrtTime) {
+		this.part1SrtTime = part1SrtTime;
+	}
+
+	public String getPart1EndTime() {
+		return part1EndTime;
+	}
+
+	public void setPart1EndTime(String part1EndTime) {
+		this.part1EndTime = part1EndTime;
+	}
+
+	public String getPart2SrtTime() {
+		return part2SrtTime;
+	}
+
+	public void setPart2SrtTime(String part2SrtTime) {
+		this.part2SrtTime = part2SrtTime;
+	}
+
+	public String getPart2EndTime() {
+		return part2EndTime;
+	}
+
+	public void setPart2EndTime(String part2EndTime) {
+		this.part2EndTime = part2EndTime;
+	}
+
+	public String getPart3SrtTime() {
+		return part3SrtTime;
+	}
+
+	public void setPart3SrtTime(String part3SrtTime) {
+		this.part3SrtTime = part3SrtTime;
+	}
+
+	public String getPart3EndTime() {
+		return part3EndTime;
+	}
+
+	public void setPart3EndTime(String part3EndTime) {
+		this.part3EndTime = part3EndTime;
+	}
+	
+	
+}
